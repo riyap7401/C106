4c281a011a731a3751b4fe4c144850075e685162
riyap7401 <128242270+riyap7401@users.noreply.github.com>
2024-01-13 10:32:40 -0600

    Add files via upload

---
+++ text.js
@@ -0,0 +1,19 @@
+https://teachablemachine.withgoogle.com/models/PvElK2Zlp/
+
+function startClassification()
+
+{
+
+   navigator.mediaDevices.getUserMedia({audio:true});
+
+   classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/PvElK2Zlp/model.json', modelReady);
+
+}
+
+function modelReady()
+
+{
+
+   classifier.classify(gotResults);
+
+}
